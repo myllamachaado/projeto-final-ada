b646c026d559b3ddd89801b9a408c2ebfbb5dfe1
luliduma <myllamachaado@gmail.com>
2023-08-23 22:47:58 -0300

    Ajuste de código

--- script_s3_ada.py
+++ script_s3_ada.py
@@ -2,33 +2,34 @@ from datetime import datetime
 import os
 import boto3
 
+BUCKET_NAME = "projeto-final-ada"
+
 
 def send_files():
     # variáveis AWS
     client = boto3.client('s3')
-    bucket = 'projeto-final-ada'
 
     # Paths
-    cur_path = os.path.join(os.getcwd(), 'files')
-    logs_path = os.path.join(os.getcwd(), 'log_integracao')
-
+    path = os.getcwd()
     arquivos = os.listdir('files')
 
     if len(arquivos) <= 0:
         print('Não existem relatórios pendentes para integração!')
     else:
         for arq in arquivos:
-            filename = os.path.join(cur_path, arq)
-            data = open(filename, 'rb')
-            # Envia itens para o S3
-            client.upload_file(filename, bucket, arq)
-            data.close()
+            filename = os.path.join(path, 'files', arq)
+            with open(filename, 'rb') as data:
+                # Envia itens para o S3
+                client.upload_file(filename, BUCKET_NAME, arq)
+
+            # Guarda a data e hora da integração
             now = str(datetime.now()).replace(':', '').replace('.', '').replace(' ', '')
-            os.rename(os.path.join(cur_path, arq), os.path.join(logs_path, f'{now}_{arq}'))
+
+            # Move o arquivo da pasta files para logs
+            os.rename(filename, os.path.join(path, 'log_integracao', f'{now}_{arq}'))
             print(f'Arquivo {arq} integrado e movido para a pasta de logs!')
 
         client.close()
-
         print('Itens integrados com sucesso!')
 
 
